cd457e5f7e39b4dee65061ba04978e61dda48d00
zyfyy <zyfyus@outlook.com>
2021-07-07 15:58:28 +0800

    use gql sample

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     siteMetadata: {
+        title: 'My Github Explore',
         siteUrl: `https://www.yourdomain.tld`
     },
     plugins: [
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,23 +1,44 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 // TODO gatsby-plugin-scss-typescript does not generat .d.ts file
-import { container } from './layout.module.css';
+import {
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText,
+    siteTitle
+} from './layout.module.css';
 
 const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `);
     return (
         <main className={container}>
-            <title>My Github Explore - {pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <p className={siteTitle}>{data.site.siteMetadata.title}</p>
             <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>
+                            Home
+                        </Link>
                     </li>
-                    <li>
-                        <Link to="/React">React Topic</Link>
+                    <li className={navLinkItem}>
+                        <Link to="/React" className={navLinkText}>
+                            React Topic
+                        </Link>
                     </li>
                 </ul>
             </nav>
-            <h1>{pageTitle}</h1>
+            <h1 className={heading}>{pageTitle}</h1>
             {children}
         </main>
     );
